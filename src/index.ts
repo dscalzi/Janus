5577593eb1c7ca6211ac0c6277775cde32e00046
Daniel Scalzi <d_scalzi@yahoo.com>
2019-07-21 00:58:01 -0400

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config()
+
+const app = express()
+
+const SERVER_PORT = process.env.SERVER_PORT || '8080'
+const BASE_PATH = process.env.BASE_PATH
+
+app.get('*', (req, res) => {
+    if (/\.\.\/?/.test(req.url)) {
+        res.sendStatus(403)
+    } else {
+        res.download(`${BASE_PATH}${req.url}`)
+    }
+})
+
+const server = app.listen(SERVER_PORT, () => {
+    console.log(`Janus started with base path ${BASE_PATH} on port ${SERVER_PORT}`)
+})
